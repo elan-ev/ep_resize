0c49e255ef7fddd93f459a87619f76b9ec7a4125
Marcus Eibrink-Lunzenauer <gitlab+elanev@luniki.de>
2019-04-02 12:03:01 +0200

    prettify plugin

--- static/js/index.js
+++ static/js/index.js
@@ -1,54 +1,73 @@
-'use strict';
+"use strict";
 
 var lastHeight;
 var lastWidth;
-exports.aceEditEvent = function (event, args, callback) {
-    var editbar = $('#editbar');
+exports.aceEditEvent = function(event, args, callback) {
+  var editbar = $("#editbar");
 
-    var elem = $('iframe[name=ace_outer]').contents().find('iframe[name=ace_inner]');
-    var newHeight = elem.outerHeight() + (editbar.length ? editbar.outerHeight() : 0);
-     var newWidth = elem.outerWidth();
+  var elem = $("iframe[name=ace_outer]")
+    .contents()
+    .find("iframe[name=ace_inner]");
+  var newHeight =
+    elem.outerHeight() + (editbar.length ? editbar.outerHeight() : 0);
+  var newWidth = elem.outerWidth();
 
-    // Get an array of all element heights
-    var elementHeights = $('iframe[name=ace_outer]').contents().find('body').children().map(function() {
-	if ($(this).is(":visible")) {
-	    return $(this).offset().top + $(this).outerHeight();
-	}
-    }).get();
+  // Get an array of all element heights
+  var elementHeights = $("iframe[name=ace_outer]")
+    .contents()
+    .find("body")
+    .children()
+    .map(function() {
+      if ($(this).is(":visible")) {
+        return $(this).offset().top + $(this).outerHeight();
+      }
+    })
+    .get();
 
-    elementHeights.push(newHeight);
-    // Math.max takes a variable number of arguments
-    // `apply` is equivalent to passing each height as an argument
-    var maxHeight = Math.max.apply(null, elementHeights);
-    if (!lastHeight || !lastWidth || lastHeight !== maxHeight || lastWidth !== newWidth) {
-	sendResizeMessage(newWidth, maxHeight);
-    }
-    
+  elementHeights.push(newHeight);
+  // Math.max takes a variable number of arguments
+  // `apply` is equivalent to passing each height as an argument
+  var maxHeight = Math.max.apply(null, elementHeights);
+  if (
+    !lastHeight ||
+    !lastWidth ||
+    lastHeight !== maxHeight ||
+    lastWidth !== newWidth
+  ) {
+    sendResizeMessage(newWidth, maxHeight);
+  }
 };
 
-exports.goToRevisionEvent = function (hook_name, context, cb) {
-    
-    var editbar = $('#timeslider-top')
-    var elem = $('#padeditor');
+exports.goToRevisionEvent = function(hook_name, context, cb) {
+  var editbar = $("#timeslider-top");
+  var elem = $("#padeditor");
 
-    var newHeight = elem.outerHeight() + (editbar.length ? editbar.outerHeight() : 0);
-    var newWidth = elem.outerWidth();
+  var newHeight =
+    elem.outerHeight() + (editbar.length ? editbar.outerHeight() : 0);
+  var newWidth = elem.outerWidth();
 
-    if (!lastHeight || !lastWidth || lastHeight !== newHeight || lastWidth !== newWidth) {
-        sendResizeMessage(newWidth, newHeight);
-    }
+  if (
+    !lastHeight ||
+    !lastWidth ||
+    lastHeight !== newHeight ||
+    lastWidth !== newWidth
+  ) {
+    sendResizeMessage(newWidth, newHeight);
+  }
 };
 
-var sendResizeMessage = function (width, height) {
-    lastHeight = height;
-    lastWidth = width;
-    
+var sendResizeMessage = function(width, height) {
+  lastHeight = height;
+  lastWidth = width;
 
-    window.parent.postMessage({
-        name: 'ep_resize',
-        data: {
-            width:  width,
-            height: height
-        }
-    }, '*');
-} 
+  window.parent.postMessage(
+    {
+      name: "ep_resize",
+      data: {
+        width: width,
+        height: height
+      }
+    },
+    "*"
+  );
+};
